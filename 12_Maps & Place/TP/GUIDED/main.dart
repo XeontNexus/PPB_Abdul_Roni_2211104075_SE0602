eeffe48ae2d24ce181c99ab79d947eceb561d615
Abdul Roni <118911383+XeontNexus@users.noreply.github.com>
2024-12-14 23:42:09 +0700

    Update main.dart

--- 12_Maps & Place/TP/GUIDED/main.dart
+++ 12_Maps & Place/TP/GUIDED/main.dart
@@ -1,30 +1,23 @@
 import 'package:flutter/material.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:modul_12/homepage.dart';
 
-class MyMaps extends StatefulWidget {
-  const MyMaps({super.key});
-
-  @override
-  State<MyMaps> createState() => _MyMapsState();
+void main() {
+  runApp(const MyApp());
 }
 
-class _MyMapsState extends State<MyMaps> {
-  static final LatLng _kMapCenter =
-      LatLng(19.018255973653343, 72.84793849278007);
-  static final CameraPosition _kInitialPosition = CameraPosition(
-    target: _kMapCenter,
-    zoom: 11.0,
-  );
-  @override
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  
+  @override 
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Google Maps Demo'),
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
       ),
-      body: GoogleMap(
-        initialCameraPosition: _kInitialPosition,
-        myLocationEnabled: true,
-      ),
+      home: MyMaps(),
     );
   }
 }
